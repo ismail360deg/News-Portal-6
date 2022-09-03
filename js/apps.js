96da693a25b5718c1989a6791b4241dc969d2503
ismail360deg <ismailbhuiyan2000@gmail.com>
2022-09-03 14:25:24 +0600

    update app.js again

--- js/apps.js
+++ js/apps.js
@@ -1,25 +1,26 @@
 const breakingNews = async () => {
-    const url = `https://openapi.programming-hero.com/api/news/category/01`
+    const url = `https://openapi.programming-hero.com/api/news/categories`
     const res = await fetch(url);
     const data = await res.json();
-    displayNews(data.data)
+
+    // console.log(data.data.news_category);
+
+    displayNews(data.data.news_category)
 }
 
-const displayNews = phones => {
-    const phonesContainer = document.getElementById('news-container');
-    phones.forEach(phone => {
-        const PhoneDiv = document.createElement('div');
-        PhoneDiv.classList.add('col');
-        PhoneDiv.innerHTML = `
-        <div class="card">
-        <img src="${phone.image_url}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${phone.title}</h5>
-            <p class="card-text">${phone.details}</p>
-        </div>
-    </div>
+const displayNews = AllNews => {
+
+
+    const newsContainer = document.getElementById('catagory-container');
+    AllNews.forEach(News => {
+        // console.log(phone)
+        const { category_name } = News
+        const NewsDiv = document.createElement('li');
+        NewsDiv.classList.add('nav-item');
+        NewsDiv.innerHTML = `
+        <a class="nav-link" href="#">${category_name}</a>
         `;
-        phonesContainer.appendChild(PhoneDiv);
+        newsContainer.appendChild(NewsDiv);
 
     })
 }
